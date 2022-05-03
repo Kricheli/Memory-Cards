b91e081a5a586b90731a791025af06005764bc6e
OrEliyahu43 <eurekas300@gmail.com>
2022-05-03 12:16:01 +0300

    second

--- scipt/cards.js
+++ scipt/cards.js
@@ -1,9 +1,15 @@
 import {generateCards as generateCards , cardArrayOptions as cardArrayOptions , shuffle as shuffle, drawCards as drawCards} from './game.js'
 
-const gameCards =  generateCards(cardArrayOptions);
-
- const tableGame = document.querySelector('.grid');
+let gameCards = generateCards(cardArrayOptions);
+const newGame = document.querySelector('button');
+newGame.addEventListener('click', restart);
+const tableGame = document.querySelector('.grid');
 
 drawCards(gameCards, tableGame);
 
-module.exports = tableGame;
+
+function restart() {
+    tableGame.innerHTML = '';
+    gameCards = generateCards(cardArrayOptions)
+    drawCards(gameCards, tableGame)
+}
