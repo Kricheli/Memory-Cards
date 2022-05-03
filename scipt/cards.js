a8814a17b20ad5ee5300087555e9564e3a2adce7
OrEliyahu43 <eurekas300@gmail.com>
2022-05-03 16:30:09 +0300

    third

--- scipt/cards.js
+++ scipt/cards.js
@@ -1,4 +1,4 @@
-import {generateCards as generateCards , cardArrayOptions as cardArrayOptions , shuffle as shuffle, drawCards as drawCards} from './game.js'
+import {startTimer as startTimer,   generateCards as generateCards , cardArrayOptions as cardArrayOptions , shuffle as shuffle, drawCards as drawCards} from './game.js'
 
 let gameCards = generateCards(cardArrayOptions);
 const newGame = document.querySelector('button');
@@ -10,6 +10,7 @@ drawCards(gameCards, tableGame);
 
 function restart() {
     tableGame.innerHTML = '';
-    gameCards = generateCards(cardArrayOptions)
-    drawCards(gameCards, tableGame)
+    gameCards = generateCards(cardArrayOptions);
+    drawCards(gameCards, tableGame);
+    startTimer();
 }
